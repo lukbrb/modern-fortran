0324b1e0665ae09ee770a948633ec5ad3ee5d87d
lukbrb <lucasbarbr@outlook.com>
2024-01-07 11:38:00 +0100

    ajout renversement temporel du tableau 'time'

--- stocks-analysis/app/stock_crossover.f90
+++ stocks-analysis/app/stock_crossover.f90
@@ -1,22 +1,27 @@
 program stock_crossover
     use mod_array, only: crossneg, crosspos, reverse
-    use mod_io, only: read_stocks, write_stock
+    use mod_io, only: read_stocks
+
     implicit none
     
     integer :: i, n, fileunit
     character(4), allocatable :: symbols(:)
     character(len=:), allocatable :: time(:)
     integer, allocatable :: buy(:), sell(:)
-    real, allocatable :: open(:), high(:), low(:), close(:), adjclose(:), volume(:)
-    real :: gain
+    real, allocatable :: open_(:), high(:), low(:), close_(:), adjclose(:), volume(:)
   
   
     symbols = ['AAPL', 'AMZN', 'CRAY', 'CSCO', 'HPQ ', &
                'IBM ', 'INTC', 'MSFT', 'NVDA', 'ORCL']
+
+
     do n = 1, size(symbols)
-        print *, "Working on ", symbols(n)
+        print *, "Processing moving average crossover for " // symbols(n) // "..."
+
         call read_stocks('data/' // trim(symbols(n)) // '.csv', &
-                        time, open, high, low, close, adjclose, volume)
+                        time, open_, high, low, close_, adjclose, volume)
+
+        time = time(size(time):1:-1)
 
         adjclose = reverse(adjclose)
 
